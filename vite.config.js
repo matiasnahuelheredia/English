5ff5ea2a3c64236e839338152a82b64b341e3e08
matiasnahuelheredia <matiasnahuelheredia@gmail.com>
2025-12-01 10:22:34 -0300

    Update base path to English with capital E

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/english/',
+  base: process.env.NODE_ENV === 'production' ? '/English/' : '/',
 })
